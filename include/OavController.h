6956c5326f80afb84716546e05c58776ac0f8d69
nesetaydinn <nesataydin1669@gmail.com>
2021-11-01 09:12:50 +0300

    Added subscribers and publishers

--- include/OavController.h
+++ include/OavController.h
@@ -1,4 +1,5 @@
 #include "ros/ros.h"
+#include "stdint.h"
 #include "geometry_msgs/Twist.h"
 #include "nav_msgs/Odometry.h"
 #include "tf/tfMessage.h"
@@ -14,22 +15,36 @@ Publish:
 
 */
 
+#define DEBUG 1
+typedef struct{
+   uint8_t x;
+   uint8_t y;
+   uint8_t z;
+}cmdVelType;
+
 class OavController
 {
 private:
         ros::NodeHandle nh_;
         ros::NodeHandle priv_nh_;
         
-        ros::Subscriber cmd_vel_;
-        ros::Publisher odom_;
-        ros::Publisher tf_;
-        ros::Publisher published_cmd_;
+        ros::Subscriber cmd_vel_sub_;
+        
+        ros::Publisher odom_pub_;
+        ros::Publisher tf_pub_;
+        ros::Publisher published_cmd_pub_;
 
         std::string robot_id_;
         std::string cmd_vel_topic_;
         std::string odom_topic_;
         std::string tf_topic_;
         std::string published_cmd_topic_;
+
+        cmdVelType lineer_cmd_;
+        cmdVelType angular_cmd_;
+
+
+    void cmdVelCallback(const geometry_msgs::Twist::ConstPtr &msg);
 
 protected:
 
--- src/OavController.cpp
+++ src/OavController.cpp
@@ -12,9 +12,26 @@ OavController::OavController(ros::NodeHandle &nh,ros::NodeHandle &priv_nh):
     tf_topic_ = priv_nh.param<std::string>("tf", "/tf");
     published_cmd_topic_ = priv_nh.param<std::string>("publish_cmd", "publish_cmd");
     /* Subscribers */
-    
+    cmd_vel_sub_=nh.subscribe<geometry_msgs::Twist>(cmd_vel_topic_,1,&OavController::cmdVelCallback,this);
     /* Publishers */
+    odom_pub_=priv_nh_.advertise<nav_msgs::Odometry>("/" + robot_id_ + "/" + odom_topic_, 1);
+    tf_pub_=priv_nh_.advertise<tf::tfMessage>("/" + robot_id_ + "/" + tf_topic_, 1);
+    published_cmd_pub_=priv_nh_.advertise<geometry_msgs::TwistStamped>("/" + robot_id_ + "/" + published_cmd_topic_, 1);
+}
 
+void OavController::cmdVelCallback(const geometry_msgs::Twist::ConstPtr &msg){
+    lineer_cmd_.x = msg->linear.x;
+    lineer_cmd_.y = msg->linear.y;
+    lineer_cmd_.z = msg->linear.z;
+    
+    angular_cmd_.x = msg->angular.x;
+    angular_cmd_.y = msg->angular.y;
+    angular_cmd_.z = msg->angular.z;
+
+    #if DEBUG
+    ROS_INFO("lineer velocity:\n x = %d\ty = %d\tz = %d",lineer_cmd_.x,lineer_cmd_.y,lineer_cmd_.z);
+    ROS_INFO("angular velocity:\n x = %d\ty = %d\tz = %d",angular_cmd_.x,angular_cmd_.y,angular_cmd_.z);
+    #endif
 }
 
 OavController::~OavController()
