402190bcc907bc94c29fb8854cae100b88fcedcd
nesetaydinn <nesataydin1669@gmail.com>
2021-11-01 16:27:28 +0300

    modied OavController.h

--- include/OavController.h
+++ include/OavController.h
@@ -16,11 +16,18 @@ Publish:
 */
 
 #define DEBUG 1
+#define FREQ_TO_PER(x) ((double)(1/x))
+
+#define ODOM_PUB_FREQ_ 50L
+#define TF_PUB_FREQ_ 50L
+#define PUBLISHED_CMD_PUB_FREQ_ 50L 
+
 typedef struct{
    uint8_t x;
    uint8_t y;
    uint8_t z;
 }cmdVelType;
+
 
 class OavController
 {
@@ -29,10 +36,14 @@ private:
         ros::NodeHandle priv_nh_;
         
         ros::Subscriber cmd_vel_sub_;
-        
+
         ros::Publisher odom_pub_;
         ros::Publisher tf_pub_;
         ros::Publisher published_cmd_pub_;
+
+        nav_msgs::Odometry odom_pub_msg_;
+        tf::tfMessage tf_pub_msg_;
+        geometry_msgs::TwistStamped published_cmd_pub_msg_;
 
         std::string robot_id_;
         std::string cmd_vel_topic_;
@@ -43,8 +54,21 @@ private:
         cmdVelType lineer_cmd_;
         cmdVelType angular_cmd_;
 
+        ros::Timer odom_pub_timer_;
+        ros::Timer tf_pub_timer_;
+        ros::Timer published_cmd_pub_timer_;
+
+
 
     void cmdVelCallback(const geometry_msgs::Twist::ConstPtr &msg);
+    
+    void odomPubTimerCallback(const ros::TimerEvent &event);
+    void tfPubTimerCallback(const ros::TimerEvent &event);
+    void publishedCmdPubTimerCallback(const ros::TimerEvent &event);
+
+    void odomPublisher(void);
+    void tfPublisher(void);
+    void publishedCmdPublisher(void);
 
 protected:
 
--- src/OavController.cpp
+++ src/OavController.cpp
@@ -11,14 +11,22 @@ OavController::OavController(ros::NodeHandle &nh,ros::NodeHandle &priv_nh):
     odom_topic_ = priv_nh.param<std::string>("odom", "odom");
     tf_topic_ = priv_nh.param<std::string>("tf", "/tf");
     published_cmd_topic_ = priv_nh.param<std::string>("publish_cmd", "publish_cmd");
+
     /* Subscribers */
     cmd_vel_sub_=nh.subscribe<geometry_msgs::Twist>(cmd_vel_topic_,1,&OavController::cmdVelCallback,this);
+
     /* Publishers */
     odom_pub_=priv_nh_.advertise<nav_msgs::Odometry>("/" + robot_id_ + "/" + odom_topic_, 1);
     tf_pub_=priv_nh_.advertise<tf::tfMessage>("/" + robot_id_ + "/" + tf_topic_, 1);
     published_cmd_pub_=priv_nh_.advertise<geometry_msgs::TwistStamped>("/" + robot_id_ + "/" + published_cmd_topic_, 1);
+
+    /* Timers */
+    odom_pub_timer_= nh.createTimer(ros::Duration(FREQ_TO_PER(ODOM_PUB_FREQ_)),&OavController::odomPubTimerCallback);
+    tf_pub_timer_= nh.createTimer(ros::Duration(FREQ_TO_PER(TF_PUB_FREQ_)),&OavController::odomPubTimerCallback);
+    published_cmd_pub_timer_= nh.createTimer(ros::Duration(FREQ_TO_PER(PUBLISHED_CMD_PUB_FREQ_)),&OavController::odomPubTimerCallback);
 }
 
+/* Subscribers Feedback */
 void OavController::cmdVelCallback(const geometry_msgs::Twist::ConstPtr &msg){
     lineer_cmd_.x = msg->linear.x;
     lineer_cmd_.y = msg->linear.y;
@@ -27,13 +35,42 @@ void OavController::cmdVelCallback(const geometry_msgs::Twist::ConstPtr &msg){
     angular_cmd_.x = msg->angular.x;
     angular_cmd_.y = msg->angular.y;
     angular_cmd_.z = msg->angular.z;
-
+    
     #if DEBUG
     ROS_INFO("lineer velocity:\n x = %d\ty = %d\tz = %d",lineer_cmd_.x,lineer_cmd_.y,lineer_cmd_.z);
     ROS_INFO("angular velocity:\n x = %d\ty = %d\tz = %d",angular_cmd_.x,angular_cmd_.y,angular_cmd_.z);
     #endif
 }
 
+/* Timer Feedback */
+void OavController::odomPubTimerCallback(const ros::TimerEvent &event){
+    OavController::odomPublisher();
+}
+
+void OavController::tfPubTimerCallback(const ros::TimerEvent &event){
+    OavController::tfPublisher();
+}
+
+void OavController::publishedCmdPubTimerCallback(const ros::TimerEvent &event){
+    OavController::publishedCmdPublisher();
+}
+
+/* Publishers Functions */
+
+
+void OavController::odomPublisher(void){
+    
+}
+
+void OavController::tfPublisher(void){
+
+}
+
+void OavController::publishedCmdPublisher(void){
+
+}
+
+/* Deconstructor */
 OavController::~OavController()
 {
 }
